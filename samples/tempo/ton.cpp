85b4ba2762c6d2b16fdb88e664630c1f14237e69
dioufissa <issadiouf@myyahoo.com>
2025-03-24 00:06:38 +0100

    Timer on Delay TON : new simple version

--- samples/tempo/ton.cpp
+++ samples/tempo/ton.cpp
@@ -1,28 +1,31 @@
+#include <iostream>
+#include <thread>
 #include "../../src/myplc.h"
 
 int main() {
-// Initialize
-myplc::TON ton1; //2 seconds timer on delay
-int8_t STEP_MACHINE = 0; //STEP MACHINE of the PLC
-
+// Declare value here
+myplc::TON ton1;
+int8_t STEP_MACHINE = 0;
 
 // Loop Program
 while (true) {
 
     switch (STEP_MACHINE)
     {
-    case 0:  // Init and Start Timer
-        ton1.PT = 3s;
-        ton1._IN = true;
-        ton1.execute();
-        if (ton1._Q) {
+    case 0:
+        ton1.PT(3s);     // 3seconds Timer on Delay
+        ton1.IN(true);   // Run the TON
+        if (ton1.Q()) {  // ton output is true
+            std::cout << "Tempo executed" << std::endl;
+            std::cout << "Q = " << ton1.Q() << " and ET = " << ton1.ET() << std::endl;
             STEP_MACHINE = 1;
         }
         break;
 
     case 1: // Reset Timer
-        ton1._IN = false;
-        ton1.execute();
+        std::cout << "Reset Tempo..." << std::endl;
+        ton1.IN(false);
+        std::cout << "Q = " << ton1.Q() << " and ET = " << ton1.ET() << std::endl;
         STEP_MACHINE = 0;
         break;
     
@@ -30,6 +33,7 @@ while (true) {
         break;
     }
 
+    std::this_thread::sleep_for(10ms); // 10ms cycle
 }
 return 0;
 }
